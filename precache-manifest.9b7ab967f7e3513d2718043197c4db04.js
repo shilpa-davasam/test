f15b39d111a2d48b21b957f3dc63c9cfc9a0bf1f
Shilpa Govindaraju <shilpadg18@gmail.com>
2019-10-16 17:28:56 +0530

    Updates

---
+++ precache-manifest.9b7ab967f7e3513d2718043197c4db04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31f109d020670c8067abc48b7db6e8b",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "b59170624cb8fc7d7301",
+    "url": "/test/static/css/main.61748409.chunk.css"
+  },
+  {
+    "revision": "c0e94075350cf566621b",
+    "url": "/test/static/js/2.9a6b0787.chunk.js"
+  },
+  {
+    "revision": "b59170624cb8fc7d7301",
+    "url": "/test/static/js/main.a0502d03.chunk.js"
+  },
+  {
+    "revision": "9af1539bde983bdf26c6",
+    "url": "/test/static/js/runtime-main.24a95b5f.js"
+  }
+]);
